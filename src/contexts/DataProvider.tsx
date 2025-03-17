25fbc7ca642c927c177cf641ac003cf3eabbf884
Gabriel Lemyre <gabriel.lemyre@lightspeedhq.com>
2025-03-17 17:32:08 -0400

    Fix types

--- src/contexts/DataProvider.tsx
+++ src/contexts/DataProvider.tsx
@@ -3,10 +3,11 @@ import { ReactElement, useState } from "react";
 import { DbDeck } from "../state/Deck";
 import { DbGame } from "../state/Game";
 import { DbPlayer } from "../state/Player";
+import { Year } from "../state/Year";
 import { DataContext } from "./DataContext";
 
 export function DataProvider({ children }: { children: ReactElement }) {
-  const [year, setYear] = useState("2025");
+  const [year, setYear] = useState(Year.Y2025);
   const [games, setGames] = useState<DbGame[]>([]);
   const [players, setPlayers] = useState<DbPlayer[]>([]);
   const [decks, setDecks] = useState<DbDeck[]>([]);
