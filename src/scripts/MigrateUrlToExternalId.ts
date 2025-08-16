f0976709f5786dd00752a1250908bc9dd93a4976
Gabriel Lemyre <gabriel.lemyre@lightspeedhq.com>
2025-08-15 19:40:56 -0700

    Fix compilation

--- src/scripts/MigrateUrlToExternalId.ts
+++ src/scripts/MigrateUrlToExternalId.ts
@@ -1,13 +1,10 @@
-import { DatabaseTable } from "../state/DatabaseTable";
-import { DbDeck } from "../state/Deck";
-import { getItems, updateItem } from "../utils/Firestore";
-
-export async function run() {
-  const decks = await getItems<DbDeck>(DatabaseTable.DECKS);
-  decks.forEach(async (deck) => {
-    updateItem<DbDeck>(DatabaseTable.DECKS, deck.id, {
-      ...deck,
-      externalId: deck.url?.split("/")[4],
-    });
-  });
-}
+// Outdated
+// export async function run() {
+//   const decks = await getItems<DbDeck>(DatabaseTable.DECKS);
+//   decks.forEach(async (deck) => {
+//     updateItem<DbDeck>(DatabaseTable.DECKS, deck.id, {
+//       ...deck,
+//       externalId: deck.url?.split("/")[4],
+//     });
+//   });
+// }
