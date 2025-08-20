1e526a58f7541d940a5df574d8284178d5e8ca91
Gabriel Lemyre <gabriel.lemyre@lightspeedhq.com>
2025-08-19 17:32:09 -0700

    Remove outdated script

--- src/scripts/PopulateDecks.ts
+++ src/scripts/PopulateDecks.ts
@@ -1,15 +1,11 @@
-import { DatabaseTable } from "../state/DatabaseTable";
-import { DbDeck, Deck } from "../state/Deck";
-import { parseAsJson } from "../utils/CSV";
-import { createItem } from "../utils/Firestore";
-
-export async function run() {
-  parseAsJson("decks.csv", (rows) => {
-    rows.forEach((row) => {
-      createItem<Deck, DbDeck>(DatabaseTable.DECKS, {
-        name: row.Name,
-        commander: row.Commander,
-      });
-    });
-  });
-}
+// Outdated
+// export async function run() {
+//   parseAsJson("decks.csv", (rows) => {
+//     rows.forEach((row) => {
+//       createItem<Deck, DbDeck>(DatabaseTable.DECKS, {
+//         name: row.Name,
+//         commander: row.Commander,
+//       });
+//     });
+//   });
+// }
