5dde0afa874329fbc615c461eb91198c99a69f96
Gabriel Lemyre <gabriel.lemyre@lightspeedhq.com>
2025-08-22 17:59:16 -0700

    Display new card details in details table

--- src/components/Decks/DeckDetailsTable.tsx
+++ src/components/Decks/DeckDetailsTable.tsx
@@ -1,4 +1,5 @@
 import { Strong, Table, Text } from "@radix-ui/themes";
+import { useMemo } from "react";
 import { DeckWithStats } from "../../state/Deck";
 import { getLongDateString } from "../../utils/Date";
 
@@ -7,17 +8,54 @@ type OwnProps = {
 };
 
 export function DeckDetailsTable({ deck }: OwnProps) {
+  const tutors = useMemo(() => {
+    return (
+      deck.cards?.filter((card) => card.tutor).map((card) => card.name) ?? []
+    );
+  }, [deck.cards]);
+
+  const massLandDenials = useMemo(() => {
+    return (
+      deck.cards
+        ?.filter((card) => card.massLandDenial)
+        .map((card) => card.name) ?? []
+    );
+  }, [deck.cards]);
+
+  const extraTurns = useMemo(() => {
+    return (
+      deck.cards?.filter((card) => card.extraTurns).map((card) => card.name) ??
+      []
+    );
+  }, [deck.cards]);
+
   return (
     <Table.Root variant="surface" size="1" layout="fixed">
       <Table.Body>
         <Table.Row>
           <Table.RowHeaderCell width="125px">Game changers</Table.RowHeaderCell>
           <Table.Cell>
-            <Text size="4">
-              <Strong style={{ whiteSpace: "pre-line" }}>
-                {deck.gameChangers.length ? deck.gameChangers.join("\n") : "-"}
-              </Strong>
-            </Text>
+            <CardNameList names={deck.gameChangers} />
+          </Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.RowHeaderCell width="125px">Tutors</Table.RowHeaderCell>
+          <Table.Cell>
+            <CardNameList names={tutors} />
+          </Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.RowHeaderCell width="125px">Extra turns</Table.RowHeaderCell>
+          <Table.Cell>
+            <CardNameList names={extraTurns} />
+          </Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.RowHeaderCell width="125px">
+            Mass land denial
+          </Table.RowHeaderCell>
+          <Table.Cell>
+            <CardNameList names={massLandDenials} />
           </Table.Cell>
         </Table.Row>
         <Table.Row>
@@ -72,5 +110,27 @@ export function DeckDetailsTable({ deck }: OwnProps) {
         </Table.Row>
       </Table.Body>
     </Table.Root>
+  );
+}
+
+function CardNameList({ names }: { names: string[] }) {
+  if (names.length === 0) {
+    return (
+      <Text size="2">
+        <Strong>-</Strong>
+      </Text>
+    );
+  }
+
+  return (
+    <ul style={{ listStyleType: "disc", marginLeft: "10px" }}>
+      {names.map((name) => (
+        <li>
+          <Text size="2">
+            <Strong>{name}</Strong>
+          </Text>
+        </li>
+      ))}
+    </ul>
   );
 }
